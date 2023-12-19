c546938fcb6dd59710f492b245228e30b1f90ead
faithchebet <faith.chebet@student.moringaschool.com>
2023-12-19 12:23:39 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(names ,event){
+for (let i = 0; i < names.length; i++) {
+
+return names.map(name => `Dear ${name}, You have been invited to the ${eventName}.`);}
+
+}
+
+let names = ['John','Jack', 'Ava'];
+let eventName = 'Graduationcle';
+let invitations = writeCards(names, eventName);
+
+writeCards(["John", "Jack", "Ava"], "Graduation");
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+
+countDown(10);
